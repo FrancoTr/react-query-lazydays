6d3f57fac38814118ca9c2ce98fc40fbb3161890
Franco Traverso <franco-t20@outlook.com>
2023-01-24 16:52:09 -0300

    Prefetch treatment data when loading homepage

--- client/src/components/app/Home.tsx
+++ client/src/components/app/Home.tsx
@@ -3,8 +3,10 @@ import { ReactElement } from 'react';
 import { GiFlowerPot } from 'react-icons/gi';
 
 import { BackgroundImage } from '../common/BackgroundImage';
+import { usePrefetchtreatments } from '../treatments/hooks/useTreatments';
 
 export function Home(): ReactElement {
+  usePrefetchtreatments();
   return (
     <Stack textAlign="center" justify="center" height="84vh">
       <BackgroundImage />
