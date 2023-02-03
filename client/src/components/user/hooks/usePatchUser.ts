b84c4210eaceeb8e3bc090e102a8b5d77132cf2e
Franco Traverso <franco-t20@outlook.com>
2023-02-03 19:55:44 -0300

    Update user data using optimistic update

--- client/src/components/user/hooks/usePatchUser.ts
+++ client/src/components/user/hooks/usePatchUser.ts
@@ -1,5 +1,6 @@
 import jsonpatch from 'fast-json-patch';
-import { UseMutateFunction, useMutation } from 'react-query';
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+import { queryKeys } from 'react-query/constants';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -33,15 +34,42 @@ export function usePatchUser(): UseMutateFunction<
 > {
   const { user, updateUser } = useUser();
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation(
     (newUserData: User) => patchUserOnServer(newUserData, user),
     {
+      onMutate: async (newData: User | null) => {
+        // Cancel any outgoing queries for user data, so old server data doesn't overwrite our optimistic update
+        queryClient.cancelQueries(queryKeys.user);
+
+        // snapshot of previous user value
+        const previousUserData: User = queryClient.getQueryData(queryKeys.user);
+
+        // optimistically update the cache with new user value
+        updateUser(newData);
+
+        // return context object with snapshotted value
+        return { previousUserData };
+      },
+      onError: (error, newData, previousUserDataContext) => {
+        // rollback cache to saved value
+        if (previousUserDataContext.previousUserData) {
+          updateUser(previousUserDataContext.previousUserData);
+          toast({
+            title: 'Update failed; restoring previous values',
+            status: 'warning',
+          });
+        }
+      },
       onSuccess: (userData: User | null) => {
         if (userData) {
-          updateUser(userData);
           toast({ title: 'User updated!', status: 'success' });
         }
+      },
+      onSettled: () => {
+        // invalidate user query to make sure we're in sync with server data
+        queryClient.invalidateQueries(queryKeys.user);
       },
     },
   );
